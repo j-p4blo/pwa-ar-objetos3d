bcc7aee3e3164ccf70da167621e2721250d8f959
Juanpis <150096185+j-p4blo@users.noreply.github.com>
2026-09-25 17:08:18 -0500

    Cache Storage: put() manual para íconos + versionado atlas3d-v2

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,35 @@
-const CACHE_NAME = 'atlas3d-v1';
+const CACHE_NAME = 'atlas3d-v2';
 const CORE_ASSETS = [
   '/',
   '/index.html',
   '/manifest.json',
 ];
+const ICON_ASSETS = [
+  '/iconos/icon-192.png',
+  '/iconos/icon-512.png',
+  '/iconos/icon-512-maskable.png',
+];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
+    caches.open(CACHE_NAME).then(async cache => {
+      await cache.addAll(CORE_ASSETS);
+      for (const url of ICON_ASSETS) {
+        const respuesta = await fetch(url);
+        await cache.put(url, respuesta);
+      }
+    })
   );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(nombres =>
+      Promise.all(
+        nombres.filter(nombre => nombre !== CACHE_NAME).map(nombre => caches.delete(nombre))
+      )
+    ).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', event => {
